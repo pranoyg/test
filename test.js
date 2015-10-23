79c55a58cdb0656e069f6bea929a1582a7aaa876
pranoyg <pranoyg@alohatechnology.com>
2015-10-23 20:31:34 +0530

    create test.js
    test

---
+++ test.js
@@ -0,0 +1,27 @@
+
+function verifyAuthWebhookData(conf,io,CompareValue,IntegrationId,ProjectId,verifyAuthWebhookDataCallback) {
+	console.log("IntegrationId");
+	console.log(IntegrationId);
+	conf.UserIntegrations.findOne({_id:IntegrationId}).exec(function (err, UserIntegrationData) {  
+
+		console.log("UserIntegrationData sdsdadsada");
+		console.log(UserIntegrationData);
+		console.log("UserIntegrationData err");
+		console.log(err);
+		if (err) {
+			verifyAuthWebhookDataCallback({statusCode:100});   
+		} else { 
+			//compare
+			console.log("UserIntegrationData.authenticationData[CompareValue]")
+		console.log(UserIntegrationData.authenticationData[CompareValue]);
+		console.log("ProjectId ");
+		console.log(ProjectId);
+
+			if(UserIntegrationData.authenticationData[CompareValue] != ProjectId ){
+				verifyAuthWebhookDataCallback({statusCode:100});
+			} else {
+				verifyAuthWebhookDataCallback({statusCode:0});
+			}
+		}
+	});
+}
